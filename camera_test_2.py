3ffd3e4a2852edc2e1b45fbd44a72e5905197888
NP2420 <nat06pham@gmail.com>
2025-08-16 13:48:37 -0400

    test

--- camera_test_2.py
+++ camera_test_2.py
@@ -1,11 +1,26 @@
 from picamera2 import Picamera2, Preview
+from libcamera import controls
 import time
 
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(main={"size": (1296,972)},
-                                             sensor={'output_size': camera_config['main']['size']} )
 
+# Set main capture and preview to high resolution
+config = picam2.create_preview_configuration(
+    main={"size": (4608, 2592)},   # Full resolution for capture
+    lores={"size": (1920, 1080)}   # Higher-res preview (instead of tiny default)
+)
 picam2.configure(config)
+
+# Enable continuous autofocus
+picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+
 picam2.start_preview(Preview.QTGL)
 picam2.start()
-time.sleep(2)
+
+try:
+    while True:
+        af_state = picam2.get_controls().get("AfState", None)
+        print(f"Autofocus state: {af_state}")
+        time.sleep(1)
+except KeyboardInterrupt:
+    picam2.stop()
