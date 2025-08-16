30b2480058439cbff8370ead2d9150d2d2998108
NP2420 <nat06pham@gmail.com>
2025-08-16 12:41:46 -0400

    autofocus

--- camera_test_2.py
+++ camera_test_2.py
@@ -11,13 +11,8 @@
 # cv2.imshow("Camera", frame)
 # cv2.waitKey(0)
 
-from picamera2 import Picamera2, Preview
-import time
+from picamera2 import Picamera2
+from libcamera import controls
 picam2 = Picamera2()
-config = picam2.create_preview_configuration()
-picam2.configure(config)
-picam2.start()
-time.sleep(2)
-picam2.stop_preview()
-picam2.start_preview(True)
-time.sleep(2)
+picam2.start(show_preview=True)
+picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
