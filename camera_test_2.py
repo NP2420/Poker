787462a4d24f20575c23b60591538570d7fb57cf
NP2420 <nat06pham@gmail.com>
2025-08-16 11:17:20 -0400

    picamera2 test

---
+++ camera_test_2.py
@@ -0,0 +1,12 @@
+from picamera2 import Picamera2
+import cv2
+
+picam2 = Picamera2()
+picam2.preview_configuration.main.size = (640, 480)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.configure("preview")
+picam2.start()
+
+frame = picam2.capture_array()
+cv2.imshow("Camera", frame)
+cv2.waitKey(0)
