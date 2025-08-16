f4d1811eeb86571716b422ffee6e906ad6ab27bb
NP2420 <nat06pham@gmail.com>
2025-08-16 13:39:08 -0400

    test

--- camera_test_2.py
+++ camera_test_2.py
@@ -1,58 +1,11 @@
-# # from picamera2 import Picamera2
-# # import cv2
-
-# # picam2 = Picamera2()
-# # picam2.preview_configuration.main.size = (640, 480)
-# # picam2.preview_configuration.main.format = "RGB888"
-# # picam2.configure("preview")
-# # picam2.start()
-
-# # frame = picam2.capture_array()
-# # cv2.imshow("Camera", frame)
-# # cv2.waitKey(0)
-
-# from picamera2 import Picamera2
-# from libcamera import controls
-# import time
-
-# picam2 = Picamera2()
-
-# # Configure for preview
-# config = picam2.create_preview_configuration()
-# picam2.configure(config)
-
-# # Start camera with preview
-# picam2.start(show_preview=True)
-
-# # Enable continuous autofocus
-# picam2.set_controls({"AfMode": controls.AfModeEnum.Auto})
-
-# time.sleep(10)  # Keep it running for 10 seconds before exit
-from picamera2 import Picamera2
-from libcamera import controls
+from picamera2 import Picamera2, Preview
 import time
 
-# Initialize Picamera2
 picam2 = Picamera2()
+config = picam2.create_preview_configuration(main={"size": (1296,972)},
+                                             sensor={'output_size': camera_config['main']['size']} )
 
-# Configure camera for preview
-preview_config = picam2.create_preview_configuration()
-picam2.configure(preview_config)
-
-# Set autofocus to continuous mode
-picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
-
-# Start the camera
-picam2.start(show_preview=True)
-
-try:
-    print("Camera running. Press Ctrl+C to exit.")
-    while True:
-        # Optional: check focus state
-        af_state = picam2.get_controls().get("AfState", None)
-        print(f"Autofocus state: {af_state}")
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    print("Stopping camera...")
-    picam2.stop()
+picam2.configure(config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(2)
